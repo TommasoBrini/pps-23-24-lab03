864f2a3c82f05deb1e2ddf31e2314afee08ca127
Tommaso <tommaso.brini@studio.unibo.it>
2024-03-18 11:00:04 +0100

    Solved sequence zip

--- src/main/scala/u03/Sequences.scala
+++ src/main/scala/u03/Sequences.scala
@@ -25,7 +25,10 @@ object Sequences: // Essentially, generic linkedlists
       case Nil()                 => Nil()
 
     // Lab 03
-    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = ???
+    def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
+      case (Cons(a,b), Cons(c,d)) => Cons((a, c), zip[A,B](b,d))
+      case _ => Nil()
+    
 
     def take[A](l: Sequence[A])(n: Int): Sequence[A] = l match
       case Cons(h,t) if n > 0 => Cons(h, take[A](t)(n-1))
