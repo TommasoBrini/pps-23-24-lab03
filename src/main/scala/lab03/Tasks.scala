7db8cc9ff6698c97e64f5b9ee3e9f9b811b3394c
Tommaso <tommaso.brini@studio.unibo.it>
2024-03-21 13:59:54 +0100

    refactor Stream file

--- src/main/scala/lab03/Tasks.scala
+++ src/main/scala/lab03/Tasks.scala
@@ -120,4 +120,42 @@ object MoreSequences:
         case Cons(x, y) => y.foldLeft(f(default, x))(f) 
         case _ => default
     
-      
+// TASK 3 (streams) svolto da solo
+object Streams:
+
+  enum Stream[A]:
+    private case Empty()
+    private case Cons(head: () => A, tail: () => Stream[A])
+
+  object Stream:
+
+    def empty[A](): Stream[A] = Empty()
+
+    def cons[A](hd: => A, tl: => Stream[A]): Stream[A] =
+      lazy val head = hd
+      lazy val tail = tl
+      Cons(() => head, () => tail)
+
+    def toList[A](stream: Stream[A]): Sequence[A] = stream match
+      case Cons(h, t) => Sequence.Cons(h(), toList(t()))
+      case _ => Sequence.Nil()
+
+    def map[A, B](stream: Stream[A])(f: A => B): Stream[B] = stream match
+      case Cons(head, tail) => cons(f(head()), map(tail())(f))
+      case _ => Empty()
+
+    def filter[A](stream: Stream[A])(pred: A => Boolean): Stream[A] = stream match
+      case Cons(head, tail) if (pred(head())) => cons(head(), filter(tail())(pred))
+      case Cons(head, tail) => filter(tail())(pred)
+      case _ => Empty()
+
+    def take[A](stream: Stream[A])(n: Int): Stream[A] = (stream, n) match
+      case (Cons(head, tail), n) if n > 0 => cons(head(), take(tail())(n - 1))
+      case _ => Empty()
+
+    def iterate[A](init: => A)(next: A => A): Stream[A] =
+      cons(init, iterate(next(init))(next))
+
+    def takeWhile[A](stream: Stream[A])(pred: A => Boolean): Stream[A] = stream match
+      case Cons(head, tail) if pred(head()) => cons(head(), takeWhile(tail())(pred))
+      case _ => Empty()
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -103,3 +103,29 @@ class TestTask2:
         val lst = MoreSequence.Cons(3, MoreSequence.Cons(7, MoreSequence.Cons(1, MoreSequence.Cons(5, MoreSequence.Nil()))))
         assertEquals(-16, lst.foldLeft(0)(_ - _))
         assertEquals(0, lst.foldLeft(0)(_ * _))
+
+
+class TestTask3:
+    import lab03.Sequences.* 
+    import Sequence.*
+    import lab03.Streams.* 
+    import Stream.* 
+
+    @Test def testIterate(): Unit = 
+        val str1 = Stream.iterate(0)(_ + 1) // {0,1,2,3,..}
+        assertEquals(Cons(0, Cons(1, Cons(2, Cons(3, Nil())))), toList(Stream.take(str1)(4)))
+
+    @Test def testMap(): Unit =
+        val str1 = Stream.iterate(0)(_ + 1) // {0,1,2,3,..}
+        val str2 = Stream.map(str1)(_ + 1) // {1,2,3,4,..}
+        assertEquals(Cons(1, Cons(2, Cons(3, Cons(4, Nil())))), toList(Stream.take(str2)(4)))
+
+    @Test def testFilter(): Unit =
+        val str1 = Stream.iterate(0)(_ + 1) // {0,1,2,3,..}
+        val str2 = Stream.filter(str1)(x => x % 2 == 1) // {1,3,5,7,..}
+        assertEquals(Cons(1, Cons(3, Cons(5, Cons(7, Nil())))), toList(Stream.take(str2)(4)))
+
+    @Test def takeWhile(): Unit = 
+        val str1 = Stream.iterate(0)(_ + 1) // {0,1,2,3,..}
+        val str2 = Stream.takeWhile(str1)(_ < 5) // {0,1,2,3,4}
+        assertEquals(Cons(0, Cons(1, Cons(2, Cons(3, Cons(4, Nil()))))), Stream.toList(str2))
